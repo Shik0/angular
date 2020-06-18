9de3b3a3da920eb822272c1735f90c84f26c981c
Shahin Mikayilov <shahin.mikayilov@dynatrace.com>
2020-06-18 15:43:51 +0200

    changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipe: Recipe[] = [
+  recipes: Recipe[] = [
+    new Recipe('Test recipe','This is a test recipe','https://realfood.tesco.com/media/images/RFO-1400x919-AsianSalmon-9a9cf566-eaad-4107-aa79-886ec53e6b31-0-1400x919.jpg'),
     new Recipe('Test recipe','This is a test recipe','https://realfood.tesco.com/media/images/RFO-1400x919-AsianSalmon-9a9cf566-eaad-4107-aa79-886ec53e6b31-0-1400x919.jpg')
   ];
   constructor() { }
