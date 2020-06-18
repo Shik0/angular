9feeb9d0ce3d7ed7cd43ed59b073549a04419eab
Shahin Mikayilov <shahin.mikayilov@dynatrace.com>
2020-06-18 11:59:20 +0200

    ADD components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe('Test recipe','This is a test recipe','https://realfood.tesco.com/media/images/RFO-1400x919-AsianSalmon-9a9cf566-eaad-4107-aa79-886ec53e6b31-0-1400x919.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
